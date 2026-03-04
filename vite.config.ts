253873b21d9a132915bcdb5a920b5017a6885e92
omon212 <omonnuloraimkulov@gmail.com>
2026-03-04 17:28:26 +0500

    edit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
+
   return {
     plugins: [react(), tailwindcss()],
     define: {
@@ -16,8 +17,8 @@ export default defineConfig(({mode}) => {
       },
     },
     server: {
-      // HMR is disabled in AI Studio via DISABLE_HMR env var.
-      // Do not modifyâfile watching is disabled to prevent flickering during agent edits.
+      host: true, // tashqi ulanish uchun
+      allowedHosts: ['omonlogistic.uz', 'www.omonlogistic.uz'],
       hmr: process.env.DISABLE_HMR !== 'true',
     },
   };
